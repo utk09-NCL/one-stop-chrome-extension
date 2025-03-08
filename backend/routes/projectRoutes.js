12f2226e298969fe8aa00e43380d7d5f24e8fbc2
utk09-NCL <utkarsh.t0298@gmail.com>
2025-03-08 14:33:17 +0000

    feat: complete Project routes - Session 2

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -6,7 +6,7 @@ const Project = require("../models/Project");
 router.post("/", async (req, res) => {
   try {
     const newProject = await Project.create(req.body);
-    res.json(newProject);
+    res.status(201).json(newProject);
   } catch (error) {
     res.status(500).json(error);
   }
@@ -17,6 +17,40 @@ router.get("/", async (req, res) => {
   try {
     const projects = await Project.find().sort({ createdAt: -1 });
     res.json(projects);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Get Single Project
+router.get("/:id", async (req, res) => {
+  try {
+    const project = await Project.findById(req.params.id);
+    res.json(project);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Update Single Project
+router.put("/:id", async (req, res) => {
+  try {
+    const updated = await Project.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(201).json(updated);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Delete a Project
+router.delete("/:id", async (req, res) => {
+  try {
+    await Project.findByIdAndDelete(req.params.id);
+    res.json({
+      message: `Deleted project with id: ${req.params.id} successfully`,
+    });
   } catch (error) {
     res.status(500).json(error);
   }
